7284906873d75d3874684b55e6cbeb948b5b0bf0
EmreErdem <emre.erdm00@gmail.com>
2021-01-28 18:39:52 +0300

    28 ocak 2021 5.Ders sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Emre";
+            musteri1.Soyadi = "Erdem";
+            musteri1.TcNo = "123123123";
+
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Havaliajans";
+            musteri2.VergiNo = "1232131234";
+
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            Console.ReadKey();
+        }
+    }
+}
